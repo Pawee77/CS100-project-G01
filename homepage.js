e399bf546115c3018b887d119f2aeaa10ff328dc
Pruek <pruek.rue@dome.tu.ac.th>
2023-11-28 02:39:46 +0700

    first commit

---
+++ homepage.js
@@ -0,0 +1,18 @@
+let popup = document.getElementById("popup");
+let popup2 = document.getElementById("popup2");
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+
+function closepopup(){
+    popup.classList.remove("open-popup");
+}
+
+function openpopup2(){
+    popup2.classList.add("open-popup");
+}
+
+function closepopup2(){
+    popup2.classList.remove("open-popup");
+}
