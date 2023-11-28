4542770e7af130918ecd09d587978035c6b0d232
Pruek <pruek.rue@dome.tu.ac.th>
2023-11-28 13:01:16 +0700

    homepage success

--- homepage.js
+++ homepage.js
@@ -1,5 +1,6 @@
 let popup = document.getElementById("popup");
 let popup2 = document.getElementById("popup2");
+let menu = document.getElementById("menupop");
 
 function openpopup(){
     popup.classList.add("open-popup");
@@ -11,8 +12,14 @@ function closepopup(){
 
 function openpopup2(){
     popup2.classList.add("open-popup");
+    popup2.classList.add("popup2-click");
 }
 
 function closepopup2(){
     popup2.classList.remove("open-popup");
 }
+
+function menuiconforclick(x) {
+    x.classList.toggle("change");
+    menupop.classList.toggle("open-menu");
+  }
