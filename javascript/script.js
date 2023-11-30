0902c72662ade2a583df9c318aa5d46ab2b86078
Pruek <pruek.rue@dome.tu.ac.th>
2023-11-30 17:34:41 +0700

    all success

--- javascript/script.js
+++ javascript/script.js
@@ -1,218 +1,230 @@
-/*
-  File: script.js
-  Author: CS100 Team
-  Date Created: 23 July 2023
-  Copyright: CSTU
-  Description: JS code of CSTU Passport that validate with JS
-*/
 
-const config = {
-    backendUrl: "http://localhost:8000/", // Default backend URL
-  };
-  const port = 8000;
-  
-  // Function to validate Firstname and Lastname
-  function validateName() {
-    const fullnameInput = document.getElementById("fullname");
-    const names = fullnameInput.value.trim().split(" ");
-    const errorElement = document.getElementById("fullnameError");
-  
-    if (names.length !== 2) {
-      errorElement.textContent = "Please enter both your Firstname and Lastname.";
-      return false;
-    } else {
-      errorElement.textContent = ""; // Clear the error message when valid
-    }
-    return true;
+//Validate Firstname and Lastname
+function validateName(){
+  const fullnameInput = document.getElementById("fullname");
+  const names = fullnameInput.value.trim().split(" ");
+  const errorElement = document.getElementById("fullnameError");
+
+  if(names.length !== 2 ){
+    errorElement.textContent = "Please enter both your Firstname and Lastname";
+    return false;
+  } 
+  else{
+    errorElement.textContent = "";
   }
-  
-  // Function to validate Student ID
-  function validateStudentID() {
-    const studentIDInput = document.getElementById("studentID");
-    const studentIDPattern = /^\d{10}$/;
-    const errorElement = document.getElementById("studentIDError");
-  
-    if (!studentIDPattern.test(studentIDInput.value)) {
-      errorElement.textContent = "Please enter a 10-digit Student ID.";
-      return false;
-    } else {
-      errorElement.textContent = ""; // Clear the error message when valid
-    }
-    return true;
+  return true;
+
+}
+
+//validate Student ID
+function validateStudentID(){
+  const studentIDInput = document.getElementById("studentID");
+  const studentIDPattern = /^6[1-6]09\d{6}$/;
+  const errorElement = document.getElementById("studentIDError");
+
+  if(!studentIDPattern.test(studentIDInput.value)){
+    errorElement.textContent = "Please enter a 10 digit Student ID and you must be a student of the Faculty of Science currently studying. "
+    return false;
+  } else{
+    errorElement.textContent = ""; 
   }
-  
-  // Function to validate University Email
-  function validateEmail() {
-    const emailInput = document.getElementById("email");
-    const emailPattern = /^.+@dome\.tu\.ac\.th$/;
-    const errorElement = document.getElementById("emailError");
-  
-    if (!emailPattern.test(emailInput.value)) {
-      errorElement.textContent =
-        "Please provide a valid university email in the format 'xxx.yyy@dome.tu.ac.th'.";
-      return false;
-    } else {
-      errorElement.textContent = ""; // Clear the error message when valid
-    }
-    return true;
+  return true;
+
+}
+
+//Function to validate University Email
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailPattern = /^.*.\w{3,}@dome.tu.ac.th$/;
+  const errorElement = document.getElementById("emailError");
+
+  if (!emailPattern.test(emailInput.value)){
+    errorElement.textContent = "Please provide a valid university email in the format 'xxx.yyy@dome.tu.ac.th'.";
+    return false;
   }
-
-  //Function to validate phone number
-  function validatePhoneNumber(){
-    const PhoneInput = document.getElementById("Phonenumber");
-    const PhonePattern = /^\d{10}$/;
-    const errorElement = document.getElementById("PhonenumberError");
-
-    if (!PhonePattern.test(PhoneInput.value)) {
-      errorElement.textContent = "Please enter your 10-digit Phone Number";
-      return false;
-    } else {
-      errorElement.textContent = ""; // Clear the error message when valid
-    }
-    return true;
+  else {
+    errorElement.textContent = "";
   }
+  return true;
+}
 
-  //FUnction to validate activities image
-  function validateImage(){
-    const ImageInput = document.getElementById("Image");
-    const fileName = ImageInput.value;
-    const ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+//Funtion to validate phone
+function validatePhoneNumber(){
+  const PhoneInput = document.getElementById("Phonenumber");
+  const PhonePattern = /^0\d{9}$/;
+  const errorElement = document.getElementById("PhonenumberError");
 
-    if(ext =="jpg" || ext =="JPG" || ext=="gif" || ext=="GIF" || ext=="PDF" || ext=="pdf") {
-      errorElement.textContent = "";
-      return true;
-    } else {
-      errorElement.textContent = "Please upload jpg , gif , pdf only";
-      return false;
+  if (!PhonePattern.test(PhoneInput.value)) {
+    errorElement.textContent = "Please enter your valid 10-digit Phone Number";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+//Function to validate picture
+function validateImage(){
+  const imageInput = document.getElementById('Img');
+  const imagePath = imageInput.value;
+  const allowedExtension = /(\.jpg|\.jpeg|\.gif)$/i;
+  if(!allowedExtension.exec(imagePath)){
+    alert("Please upload file .jpeg/.jpg/.gif only");
+    imageInput.value = '';
+    return false;
+  }
+  else{
+    if (imageInput.files && imageInput.files[0]){
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        document.getElementById('imagePreview').innerHTML = '<img src="'+e.target.result+'" height="400"/>';
+
+      };
+      reader.readAsDataURL(imageInput.files[0]);
     }
   }
-  
-  // Function to validate form inputs on user input
-  function validateFormOnInput() {
-    validateName();
+}
+
+//Function to validate form inputs on user input
+function validateFormOnInput(){
+    alidateName();
     validateStudentID();
     validateEmail();
     validatePhoneNumber();
-    validateImage();
-  }
-  
-  // Function to fetch activity types from the backend
-  async function fetchActivityTypes() {
-    try {
-      const response = await fetch(`http://${window.location.hostname}:${port}/getActivityType`);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        console.error("Failed to fetch activity types.");
-        return [];
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching activity types:", error);
-      return [];
-    }
-  }
-  
+}
+
   // Function to populate activity types in the select element
-  function populateActivityTypes(activityTypes) {
-    const activityTypeSelect = document.getElementById("activityType");
+function populateActivityTypes(activityTypes) {
+   const activityTypeSelect = document.getElementById("activityType");
   
-    for (const type of activityTypes) {
-      const option = document.createElement("option");
-      option.value = type.id;
-      option.textContent = type.value;
-      activityTypeSelect.appendChild(option);
-    }
+  for (const type of activityTypes) {
+    const option = document.createElement("option");
+    option.value = type.id;
+    option.textContent = type.value;
+    activityTypeSelect.appendChild(option); 
   }
+}
+
+async function submitForm(event){
+  event.preventDefault();
+  const startDateInput =  document.getElementById("startDate").value;
+  const endDateInput = document.getElementById("endDate").value;
+  const startDate = new Date(startDateInput);
+  const endDate = new Date(endDateInput);
   
-  // Event listener when the page content has finished loading
-  document.addEventListener("DOMContentLoaded", async () => {
-    const activityTypes = await fetchActivityTypes();
-    populateActivityTypes(activityTypes);
-  });
-  
-  // Function to submit the form
-  // Function to submit the form
-  async function submitForm(event) {
-    event.preventDefault();
-  
-    // Validate form inputs before submission
-    if (!validateName() || !validateStudentID() || !validateEmail() || !validatePhoneNumber() || validateImage()) {
-      return;
-    }
-  
-    const startDateInput = document.getElementById("startDate").value;
-    const endDateInput = document.getElementById("endDate").value;
-    const startDate = new Date(startDateInput);
-    const endDate = new Date(endDateInput);
-  
+  errorElement = document.getElementById("submitError");
+
+  if(!validateName() || !validateStudentID() || !validateEmail() || !validatePhoneNumber() || endDate <= startDate){
     if (endDate <= startDate) {
       alert("End datetime should be after the start datetime.");
-      return;
     }
-  
-    // Create the data object to send to the backend
-    const formData = new FormData(event.target);
-    const data = {
-      first_name: formData.get("fullname").split(" ")[0],
-      last_name: formData.get("fullname").split(" ")[1],
-      student_id: parseInt(formData.get("studentID")),
-      email: formData.get("email"),
-      title: formData.get("workTitle"),
-      phone: parseInt(formData.get("Phonenumber")),
-      type_of_work_id: parseInt(formData.get("activityType")),
-      academic_year: parseInt(formData.get("academicYear")) - 543,
-      semester: parseInt(formData.get("semester")),
-      start_date: formData.get("startDate"),
-      end_date: formData.get("endDate"),
-      location: formData.get("location"),
-      description: formData.get("description")
-    };
-  
-    console.log(data);
-    
-    try {
-      // Send data to the backend using POST request
-      const response = await fetch(`http://${window.location.hostname}:${port}/record`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log("Form data submitted successfully!");
-  
-        // Format JSON data for display
-        const formattedData = Object.entries(responseData.data)
-          .map(([key, value]) => `"${key}": "${value}"`)
-          .join("\n");
-  
-        // Display success message with formatted data
-        alert(responseData.message + "\n" + formattedData);
-  
-        document.getElementById("myForm").reset();
-      } else {
-        console.error("Failed to submit form data.");
-  
-        // Display error message
-        alert("Failed to submit form data. Please try again.");
-      }
-    } catch (error) {
-      console.error("An error occurred while submitting form data:", error);
-    }
+    errorElement.textContent = "Please fill in complete information.";
+    return false;
   }
+  else{
+    errorElement.textContent = "";
+    const formData = new FormData(event.target);
+
+    for (const [name, value] of formData) {
+        console.log(name, value);
+      }
+
+      const fullnameInput = document.getElementById("fullname");
+      const studentIDInput = document.getElementById("studentID");
+      const emailInput = document.getElementById("email");
+      const workTitleInput = document.getElementById("workTitle");
+      const activityTypeInput = document.getElementById('activityType');
+      const academicYearInput = document.getElementById('academicYear');
+      const semesterInput = document.getElementById('semester');
+      const locationInput = document.getElementById('location');
+      const descriptionInput = document.getElementById('description');
+      const imageInput = document.getElementById('Img');
+      const data = {
+        first_name: fullnameInput.value.split(" ")[0],
+        last_name: fullnameInput.value.split(" ")[1],
+        student_id: studentIDInput.value,
+        email: emailInput.value,
+        title: workTitleInput.value,
+        type_of_work_id: activityTypeInput.value,
+        academic_year: academicYearInput.value,
+        semester: semesterInput.value,
+        start_date: startDateInput.value,
+        end_date: endDateInput.value,
+        location: locationInput.value,
+        description: descriptionInput.value,
+        image: imageInput.value
+      };
+      
+      const formDataObject = {};
+      formData.forEach((value, key) => {
+        formDataObject[key] = value;
+      });
+      var resultadd = document.getElementById("result");
+      resultadd.classList.add("resultforsai");
+      var Content = JSON.stringify(data, null, 2);
+      var divElement = document.createElement('div');
+      var divElementPic = document.createElement('div');
+      divElementPic.id = "picture";
+      
+      divElement.textContent = Content.slice(1, -1).replace(/,/g, '').replace(/"/g, '');
+      Contentforalert = Content.slice(1, -1).replace(/,/g, '').replace(/"/g, '');
+      
+      document.getElementById('result').appendChild(divElement);
+      
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        var imageElement = document.createElement('img');
+        imageElement.src = e.target.result;
+        imageElement.alt = 'Preview';
+        imageElement.style.maxHeight = '300px';
+        divElementPic.appendChild(imageElement);
+      };
+      
+      reader.readAsDataURL(imageInput.files[0]);
+      
+      document.getElementById('result').appendChild(divElementPic);
+
+      var piccss = document.getElementById("picture");
+      piccss.classList.add("piccs");
+      document.getElementById("myForm").reset();
+      
+      alert(Contentforalert);
+
+      try {
+        // Send data to the backend using POST request
+        const response = await fetch(`http://${window.location.hostname}:${port}/record`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+    
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log("Form data submitted successfully!");
+    
+          
+        } else {
+          console.error("Failed to submit form data.");
+    
+          // Display error message
+          alert("Failed to submit form data. Please try again.");
+        }
+      } catch (error) {
+        console.error("An error occurred while submitting form data:", error);
+      }
+      return true;
+  }
+}
+
+// Event listener for form submission
+document.getElementById("myForm").addEventListener("submit", submitForm);
   
-  // Event listener for form submission
-  document.getElementById("myForm").addEventListener("submit", submitForm);
-  
-  // Event listeners for input validation on user input
-  document.getElementById("fullname").addEventListener("input", validateName);
-  document
-    .getElementById("studentID")
-    .addEventListener("input", validateStudentID);
-  document.getElementById("email").addEventListener("input", validateEmail);
-  document.getElementById("Phonenumber").addEventListener("input",validatePhoneNumber);
-  document.getElementById("Img").addEventListener("input",validateImage)
+// Event listeners for input validation on user input
+document.getElementById("fullname").addEventListener("input", validateName);
+document
+  .getElementById("studentID")
+  .addEventListener("input", validateStudentID);
+document.getElementById("email").addEventListener("input", validateEmail);
+document.getElementById("Phonenumber").addEventListener("input",validatePhoneNumber);
